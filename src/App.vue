5a18887e5698830c852a9c8245b768559080df47
Arapu Trevor <105975921+Trevor-143@users.noreply.github.com>
2023-02-20 10:36:32 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <Header :totalIncome="state.totalIncome" />
+  <Form @add-income="addIncome" />
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Form from './components/Form.vue'
+import { reactive, computed } from 'vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Form
+  },
+  setup() {
+    const state = reactive({
+      income: [ {
+        value: 0
+      }],
+      totalIncome: computed(() => {
+        let temp = 0;
+        if (state.income.length > 0) {
+          for (let i = 0; i < state.income.length; i++) {
+            temp += state.income[i].value;
+          }
+        }
+        return temp;
+      })
+    })
+    const addIncome = (data) => {
+      state.income = [...state.income, {
+        id: Date.now(),
+        desc: date.desc,
+        value: date.value,
+        date: data.date
+      }]
+    }
+
+    return {
+      state
+    }
+  }
+
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+body {
+  background-color: #eee;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <header>
+        <h1>Income Tracker</h1>
+        <div class="total-income">$ {{ totalIncome }}.00</div>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+    props: {
+        totalIncome: Number
+    }
+
+}
+
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px 30px;
+    background-color: #313131;
+    border-bottom: 5px solid #ffce00;
+
+}
+header h1 {
+    color: #eee;
+    font-size: 28px;
+
+}
+header .total-income {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: #ffce00;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 900;
+    padding: 5px 10px;
+    min-width: 100px;
+    text-align: center;
+    border-radius: 8px;
+
+}
+</style>
